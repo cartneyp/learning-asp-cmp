6b41c9e5e1c4ac22527da96af359cea9fc2eac8b
cartneyp <58318461+cartneyp@users.noreply.github.com>
2019-12-03 16:15:55 +0800

    Initial Commit

---
+++ PEREZIP/PEREZIP.WebSite/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using PEREZIP.WebSite.Services;
+using PEREZIP.WebSite.Models;
+
+namespace PEREZIP.WebSite.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+
+        }
+        [HttpGet]
+        [Route("Rate")]
+        public ActionResult Get(
+            [FromQuery]string ProductId,
+            [FromQuery]int Rating)
+        {
+            ProductService.AddRating(ProductId, Rating);
+            return Ok();
+        }
+
+    }
+}
